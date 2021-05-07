2dfbe173a63fcfa98cfc1a70441d2bf36fe1eac3
Anjana Iyer <anjanai.yahoo@gmail.com>
2021-05-07 16:27:02 -0700

    Add subset ability for quiz

--- ear-training.js
+++ ear-training.js
@@ -1,3 +1,6 @@
+const yellow = 'rgb(255, 255, 0)';
+const gray = 'rgb(239, 239, 239)';
+
 const synth = new Tone.Synth().toDestination();
 
 const srgm = "s r R g G m M P d D n N S".split(' ');
@@ -13,23 +16,66 @@ var num_correct = num_total = 0;
 var got_note = false;
 var num_attempts = 0;
 var attempts = {};
+var remove_notes;
 
 $(document).ready(function () {
+    remove_notes = localStorage.getItem('removeNotes');
+    if (remove_notes === null) remove_notes = 'r,g,M,d,n'.split(',');
+    
+    $(".trigger_popup_subset").click(function(){
+       $('.hover_bkgr_subset').show();
+    });
+    
+    $('.popupCloseButton').click(function(){
+        $('.hover_bkgr_subset').hide();
+    });
+
+
     $("#repeat").hide();
     for (let i in srgm) {
 	let b = $('<button/>', {
 	    text: srgm[i],
 	    id: srgm[i],
-	    click: function () { playnote(notemap[this.id]); }
+	    click: function () { playnote(this.id); }
 	});
 	b.addClass("swar");
 	$("#hear").append(b);
 
 	b2 = b.clone();
-	b2.click (function() { checknote(this, notemap[this.id]); });
+	b2.click (function() { checknote(this); });
 	$("#quiz").append(b2);
+
+	b = $('<button/>', {
+	    text: srgm[i],
+	    id: 'A' + srgm[i],
+	    click: function () { togglenote(this); }
+	});
+	b.addClass("swar");
+	$("#popup_swaras").append(b);
     }
+
+    remove_notes = remove_notes.split(',');
+    jQuery.each(remove_notes, function(i, id) {
+	$("#"+id).hide();
+	$("#A"+id).css({
+	    'background-color': yellow,
+	    'text-decoration': 'line-through'
+	});
+    });
+
+    
 });
+
+function togglenote (note) {
+    color = $(note).css('background-color');
+    if (color == gray) $( note ).css({
+	'background-color': yellow,
+	'text-decoration': 'line-through'
+    }); else $( note ).css({
+	'background-color': gray,
+	'text-decoration': 'none'
+    });
+}
 
 function tanpura() {
     let tanpura = $('#tanpura')[0];
@@ -49,19 +95,24 @@ const sampler = new Tone.Sampler({
 }).toDestination();
 
 
-function next() {
+function start() {
     $("#repeat").show();
     $("#start").hide();
-    lastnote = abcd[Math.floor(Math.random() * abcd.length)];
+    next();
+}
+
+function next() {
+    $("#quiz").children().css('background-color', 'linen');
+    let active = srgm.filter(x => !remove_notes.includes(x));
+    lastnote = active[Math.floor(Math.random() * active.length)];
     playnote(lastnote);
     num_total++;
     got_note = false;
     num_attempts = 0;
-    $("#quiz").children().css('background-color', 'linen');
 }
 
 function playnote(note) {
-    sampler.triggerAttackRelease(note, "2n");
+    sampler.triggerAttackRelease(notemap[note], "2n");
 
 }
 
@@ -69,7 +120,8 @@ function repeat() {
     playnote(lastnote);
 }
 
-function checknote(button, note) {
+function checknote(button) {
+    let note = $(button).attr('id');
     console.log (lastnote, note);
     num_attempts ++;
     if (lastnote == note && num_attempts == 1) num_correct++;
@@ -82,7 +134,21 @@ function checknote(button, note) {
     window.setTimeout(function(){
 	if (lastnote == note) next();
     }, 900);
-
     
 
 }
+
+function createSubset() {
+    remove_notes = [];
+    $("#popup_swaras button").each(function() {
+	id = $(this).attr('id').substring(1);
+	if ($(this).css("background-color") === yellow) {
+	    $("#"+id).hide();
+	    remove_notes.push(id);
+	} else {
+	    $("#"+id).show();
+	}
+    });
+    localStorage.setItem('removeNotes', remove_notes);
+}
+
